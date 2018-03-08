be82b28d99d3eb4411c1ad54ca5e97462da59d83
Kate-Dt <dotsiuk.k@gmail.com>
2018-03-08 13:08:12 +0200

    Initial commit

---
+++ Frontend/javascript/main.js
@@ -0,0 +1,7 @@
+
+
+
+https://stackoverflow.com/questions/9376192/add-icon-to-submit-button-in-twitter-bootstrap-2
+$("#myFavoriteFormSubmitButton").bind('click', function(event) {
+    $("#myFavoriteForm").submit();
+});
