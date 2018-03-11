a881f30a84b9db5fe1d701317a92179a35671581
Kate-Dt <dotsiuk.k@gmail.com>
2018-03-11 21:22:58 +0200

    added login and register

--- Frontend/javascript/main.js
+++ Frontend/javascript/main.js
@@ -1,7 +1,22 @@
+$(document).ready(function(){
+
+    $('.messageRegister').click(function () {
+
+        $('.login-form').hide();
+        $('.register-form').show();
+    });
 
 
+    $('.messageLogin').click(function () {
 
-https://stackoverflow.com/questions/9376192/add-icon-to-submit-button-in-twitter-bootstrap-2
-$("#myFavoriteFormSubmitButton").bind('click', function(event) {
-    $("#myFavoriteForm").submit();
+        $('.login-form').show();
+        $('.register-form').hide();
+    })
+
 });
+
+
+// https://stackoverflow.com/questions/9376192/add-icon-to-submit-button-in-twitter-bootstrap-2
+// $("#myFavoriteFormSubmitButton").bind('click', function(event) {
+//     $("#myFavoriteForm").submit();
+// });
